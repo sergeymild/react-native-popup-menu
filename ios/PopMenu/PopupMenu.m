3dda94dd9e751636d90d83bf039c4979ec866996
SergeyMild <sergeymild@yandex.ru>
2022-02-27 16:23:30 +0200

    Update PopupMenu.m

--- ios/PopMenu/PopupMenu.m
+++ ios/PopMenu/PopupMenu.m
@@ -23,6 +23,7 @@ RCT_EXTERN_METHOD(dismiss)
 
 @interface RCT_EXTERN_MODULE(ScalePressViewManager, RCTViewManager)
 RCT_EXPORT_VIEW_PROPERTY(onPress, RCTBubblingEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onLongPress, RCTBubblingEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(scale, NSNumber)
 RCT_EXPORT_VIEW_PROPERTY(durationIn, NSNumber)
 RCT_EXPORT_VIEW_PROPERTY(durationOut, NSNumber)
